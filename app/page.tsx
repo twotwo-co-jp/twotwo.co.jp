919c45d01033a16167d1d8150efffca8b3adaff0
tzz <tzz.zhao@gmail.com>
2025-01-27 15:29:20 +0900

    パフォーマンス改善

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Image
-        src="/twotwo-logo.png"
-        alt="logo"
+        src="/twotwo-logo.webp"
+        alt="TWOTWO logo"
         width={200}
         height={200}
-        loading="eager"
+        priority
+        sizes="(max-width: 480px) 100px, (max-width: 768px) 150px, 200px"
+        className="max-w-full h-auto"
       />
       <p className="text-2xl md:text-4xl font-bold mt-4">{t('slogan1')}</p>
       <p className="text-2xl md:text-4xl font-bold mt-4">{t('slogan2')}</p>
