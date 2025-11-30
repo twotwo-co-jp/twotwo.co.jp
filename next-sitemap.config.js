ede8ee25d36f63891eff99c5fa952b38a5915ee9
tzz <tzz.zhao@gmail.com>
2025-11-30 22:01:37 +0900

    sitemap設定修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    siteUrl: 'https://twotwo.co.jp',
-    generateRobotsTxt: true,
-  };
+  siteUrl: "https://twotwo.co.jp",
+  generateRobotsTxt: true,
+  additionalPaths: async (config) => [
+    await config.transform(config, "/"),
+    await config.transform(config, "/about"),
+    await config.transform(config, "/services"),
+    await config.transform(config, "/contact"),
+  ],
+};
